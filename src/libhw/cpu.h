66b8fea97f197d7122c44301b14877004fadfc6f
Ali Nasrolahi <A.Nasrolahi01@gmail.com>
2024-02-27 07:50:09 +0330

    Basic project structure (devel).

---
+++ src/libhw/cpu.h
@@ -0,0 +1,10 @@
+/**
+ * @file cpu.h
+ * @author Ali Nasrolahi (a.nasrolahi01@gmail.com)
+ * @brief Main CPU structures
+ * @date 2024-02-27
+ */
+#ifndef _CPU_H_
+#define _CPU_H_
+
+#endif /* _CPU_H_ */
--- src/main.c
+++ src/main.c
@@ -3,5 +3,4 @@
  * @author Ali Nasrolahi (a.nasrolahi01@gmail.com)
  * @date 2024-02-25
  */
-
 int main(int argc, char **argv) { return 0; }

07bc0b8defeedf26d733e78aaed5ffe0d1567a19
Ali Nasrolahi <A.Nasrolahi01@gmail.com>
2024-02-27 11:52:51 +0330

    Compile fix (devel).

--- src/libhw/cpu.c
+++ src/libhw/cpu.c
@@ -1,15 +1,16 @@
 #include "cpu.h"
-#include <unistd.h>
+
+#include <stdint.h>
+#include <string.h>
 
 static int cpu_open(void);
 static int cpu_read(void *buf, uint16_t size);
 static int cpu_write(void *buf, uint16_t size);
-static int close();
+static int cpu_close(void);
 
 int init_cpu(struct bus_type *bus, CPU *cpu)
 {
     memset(cpu, 0, sizeof(CPU));
     cpu->dev.name = "CPU";
-    cpu->dev.open;
     return 0;
 }
